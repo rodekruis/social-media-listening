f5f1f47a9f819d51affd1a86ba631e7a6a189622
Wessel de Jong <wes_dejong9@hotmail.com>
2022-06-09 13:27:43 +0200

    Filter keywords based on keyword csv files

--- pipeline/src/pipeline/parse_data.py
+++ pipeline/src/pipeline/parse_data.py
@@ -284,31 +284,24 @@ def parse_telegram(config):
     df_messages = pd.read_csv(messages_path)
     df_messages['date'] = pd.to_datetime(df_messages['datetime']).dt.strftime('%Y-%m-%d')
     next_text_value = 'full_text_en'
-    topic_1 = 'ru_uk'
-    topic_2 = 'cva'
-    topic_3 = 'pgi'
 
     # get distribution of words
     if config["get-word-freq"]:
         get_word_frequency(df_messages, next_text_value)
 
     if config["filter-by-keywords"]:
-        # filter 1: by keywords RED CROSS in UK and RU
-        df_keywords_1 = pd.read_csv(f'../config/{config["keywords-1-filename"]}')
-        keywords_1 = df_keywords_1.dropna()['keyword'].tolist()
-        df_messages = filter_by_keywords(df_messages, [next_text_value], keywords_1, topic_1)
-        # filter 2: by keywords CVA in UK and RU
-        df_keywords_2 = pd.read_csv(f'../config/{config["keywords-2-filename"]}')
-        keywords_2 = df_keywords_2.dropna()['keyword'].tolist()
-        df_messages = filter_by_keywords(df_messages, [next_text_value], keywords_2, topic_2)
-        # filter 3: by keywords PGI in UK and RU
-        df_keywords_3 = pd.read_csv(f'../config/{config["keywords-3-filename"]}')
-        keywords_3 = df_keywords_3.dropna()['keyword'].tolist()
-        df_messages = filter_by_keywords(df_messages, [next_text_value], keywords_3, topic_3)
+        keyword_files = config["keyword-files"]
+
+        for file in keyword_files:
+            topic = file.split("_")[0]
+
+            df_keywords = pd.read_csv(f"../config/{file}")
+            keywords = df_keywords.dropna()['keyword'].tolist()
+            df_messages = filter_by_keywords(df_messages, [next_text_value], keywords, topic)
 
     # translate telegram messages
     if config["translate"]:
-        df_to_translate = df_messages[(df_messages[topic_1]) | (df_messages[topic_3])]
+        df_to_translate = df_messages[(df_messages["rcrc"])]
         df_messages = translate_dataframe(df_to_translate, next_text_value, 'full_text_en', config)
         next_text_value = 'full_text_en'
     # save_data("messages_translated", "telegram", df_messages, "id", config)
@@ -331,16 +324,16 @@ def parse_telegram(config):
     if config["analyse-topic"]:
         # analyse topic RED CROSS and CVA
         df_messages_1 = df_messages[
-            (df_messages[topic_1]) & \
-            (df_messages[topic_2]) & \
-            (df_messages['date'] >= '2022-04-28')
+            (df_messages["rcrc"]) &
+            (df_messages["cva"]) &
+            (pd.to_datetime(df_messages['date']) >= pd.to_datetime('2022-04-28'))
             ]
-        df_messages_1 = predict_topic(df_messages_1, next_text_value, config, topic_1)
+        df_messages_1 = predict_topic(df_messages_1, next_text_value, config, "cva")
         # analyse topic RED CROSS and not CVA
-        df_messages_2 = df_messages[df_messages[topic_1] & ~df_messages[topic_2]]
-        df_messages_2 = predict_topic(df_messages_2, next_text_value, config, topic_2)
-        # analyse topic PGI
-        df_messages_3 = df_messages[df_messages[topic_3]]
+        df_messages_2 = df_messages[(df_messages["rcrc"]) & (~df_messages["cva"])]
+        df_messages_2 = predict_topic(df_messages_2, next_text_value, config, "rcrc")
+        # # analyse topic PGI
+        df_messages_3 = df_messages[df_messages["pgi"]]
         df_messages_3 = predict_topic(df_messages_3, next_text_value, config, topic_3)
         # merge all into one single df
         df_messages = df_messages_1.append(df_messages_2, ignore_index=True)
