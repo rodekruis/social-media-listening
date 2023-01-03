42c8d5935d85d34f75bf2de937b7e1b19cc72f24
jmargutti <jacopo.margutti@gmail.com>
2023-01-03 19:01:15 +0100

    fix datetime typo, implement methods for translations and classifications

--- smm/src/smm/message.py
+++ smm/src/smm/message.py
@@ -15,12 +15,12 @@ class Message:
                  reply=False,
                  reply_text=None,
                  reply_to=None,
-                 translation=None,
-                 info=None, # country, etc.
-                 classification=None):
+                 translations=None,
+                 info=None,  # country, etc.
+                 classifications=None):
         self.id_ = id_
         if isinstance(datetime_, datetime):
-            self.datetime_ = datetime
+            self.datetime_ = datetime_
         else:
             self.datetime_ = pd.to_datetime(datetime_)
         self.source = source
@@ -29,41 +29,51 @@ class Message:
         self.text = text
         self.reply = reply
         self.reply_to = reply_to
-        if translation is None:
-            self.translation = {}
+        if translations is None:
+            self.translations = []
         if info is None:
             self.info = {}
-        if classification is None:
-            self.classification = {}
+        if classifications is None:
+            self.classifications = []
 
     def from_twitter(self, dict_):
         # TBI automatically map from tweet
+        pass
 
     def from_telegram(self, dict_):
         # TBI automatically map from telegram
+        pass
 
     def from_kobo(self, dict_):
         # TBI automatically map from telegram
+        pass
 
     def set_translation(self, dict_):
-        # TBI check that dict_ follow structure
-        # [{"text": "...", "from": "...", "to": "..."}, ...]
+        if type(dict_) is not dict:
+            raise TypeError("Translation must be a dictionary")
+        if list(dict_.keys()) != ['text', 'from_lang', 'to_lang']:
+            raise KeyError("Translation must contain the keys 'text', 'from_lang', 'to_lang'")
+        self.translations.append(dict_)
 
-    def set_classification(self, dict_):
-        # TBI check that dict_ follow structure
-        # [{"class": "...", "score": "..."}, ...]
+    def set_classification(self, dict_list):
+        if type(dict_list) is not list:
+            raise TypeError("Classification must be a list of dictionaries")
+        for dict_ in dict_list:
+            if list(dict_.keys()) != ['class', 'score']:
+                raise KeyError("Classification must contain the keys 'class', 'score'")
+        self.classifications.extend(dict_list)
 
     def to_dict(self):
         return {
-            'id' : self.id_,
-            'datetime' : self.datetime_,
-            'source' : self.source,
-            'group' : self.group,
-            'text' : self.text,
-            'reply' : self.reply,
-            'reply_to' : self.reply_to,
-            'translation' : self.translation,
-            'info' : self.info,
-            'classification' : self.classification
+            'id': self.id_,
+            'datetime': self.datetime_,
+            'source': self.source,
+            'group': self.group,
+            'text': self.text,
+            'reply': self.reply,
+            'reply_to': self.reply_to,
+            'translations': self.translations,
+            'info': self.info,
+            'classifications': self.classifications
         }
 
